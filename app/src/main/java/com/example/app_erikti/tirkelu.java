30169a339a557242a50b59c8f56a8bf44bd23fca
atibek0va <togzhanatibekova@gmail.com>
2020-11-22 09:46:37 +0600

    tirkelu

--- app/src/main/java/com/example/app_erikti/tirkelu.java
+++ app/src/main/java/com/example/app_erikti/tirkelu.java
@@ -1,36 +1,34 @@
 package com.example.app_erikti;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class tirkelu extends AppCompatActivity implements View.OnClickListener{
     private ImageButton btnNews;
     private ImageButton btnMap;
     private ImageButton btnUser;
-    private Button btnTirkelu;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_tirkelu);
         btnNews = findViewById(R.id.btnNews);
         btnMap = findViewById(R.id.btnMap);
         btnUser = findViewById(R.id.btnUser);
-        btnTirkelu = findViewById(R.id.btnTirkelu);
+
 
         btnNews.setOnClickListener(this);
         btnMap.setOnClickListener(this);
         btnUser.setOnClickListener(this);
-        btnTirkelu.setOnClickListener(this);
+
     }
+
 
     @Override
     public void onClick(View v) {
@@ -42,14 +40,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent map_page = new Intent(this,mapPage.class);
             startActivity(map_page);
         }
-        else if (v.getId() == R.id.btnTirkelu){
-            Intent tirk = new Intent(this,tirkelu.class);
-            startActivity(tirk);
-        }
+
         else {
             Intent user_page = new Intent(this,MainActivity.class);
             startActivity(user_page);
         }
+
+
 
 
     }
